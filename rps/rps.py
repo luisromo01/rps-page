39a17cf44c483b6a858a0cbaf94e84384c575c24
luisromo01 <lromo400@gmail.com>
2021-07-18 10:13:37 -0500

    Update rps.py
    added comments for future reference

--- rps/rps.py
+++ rps/rps.py
@@ -1,3 +1,7 @@
+#todo: add code for saving win/loss record using json and/or database
+#add more gui elements using css/javascript
+#make the computer AI more intelligent when making a selection
+#incorporate database by creating log in page.
 from flask import render_template, request, redirect, url_for, flash, abort, session, jsonify, Blueprint
 import json
 import os.path
@@ -5,29 +9,19 @@ from werkzeug.utils import secure_filename
 from random import randrange
 
 bp = Blueprint('rps',__name__)
-empty = True 
-#todo: add code for saving win/loss record using json and/or database
-#add more gui elements using css/javascript
-#make the computer AI more intelligent when making a selection
+empty = True #created this so that the page knows if someone has made a selection between rock, paper, or scissors yet. 
 @bp.route('/')
-def home():
-    #set something to false here. then set it to true on the post part below
+def home(): #should be the first page the users sees. The variable new is set to True and that's what home.html will use to choose what it will show the user
    return render_template('home.html', codes=session.keys(), new=empty)
 #investigate reload function for home page with if statement for if something selected or not that way, you do everything in one html page.
 @bp.route('/', methods=['GET', 'POST'])
 def selection():
-    if request.method == 'POST':
-        empty=False
-        choice_dict = {0: 'Rock', 1: 'Paper', 2: 'Scissors'}
-        cpu_chose = choice_dict[randrange(3)]
+    if request.method == 'POST': #this should come up after the user makes a selection of rock, paper, or scissor
+        empty=False #changed variable to False so that the page loads the correct part of home.html
+        choice_dict = {0: 'Rock', 1: 'Paper', 2: 'Scissors'} #mapping numbers to the possible choises.
+        cpu_chose = choice_dict[randrange(3)] #using a random selector, the computer will chose either rock, paper or scissors. The interger will act as a key to the map and the variable will save the value.
         return render_template('home.html',  selection = request.form['selection'],new = empty, cpu_select = cpu_chose)
 
-#        if request.form['submit_button'] == 'Rock':
-#            return render_template('Rock.html')
-#        elif request.form['submit_button'] == 'Paper':
-#            return render_template('Paper.html')
-#        elif request.form['submit_button'] == 'Paper':
-#            return render_template('Scissors.html')
     else:
         pass # unknown
  
